02cd0828297237d2c222687ceafe60df7517107b
arjun98k <arjunkandekar14@gmail.com>
2024-07-07 17:07:11 +0530

    arrays

---
+++ Var_basic.java
@@ -0,0 +1,9 @@
+public class Var_basic {
+    public static void main(String[] args) {
+        int arr [] = {11,87,90,46};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}

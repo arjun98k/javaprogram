685959cff2144aa166f0a81c953fb4c9de826872
arjun98k <arjunkandekar14@gmail.com>
2024-08-07 20:06:37 +0530

    arrays

---
+++ Opps.java
@@ -0,0 +1,12 @@
+public class Opps {
+
+    public int Add(int a , int b){
+        int c = a+b;
+        return c;
+    }
+    public static void main(String[] args) {
+        
+        Opps A1 = new Opps();
+       System.out.println( A1.Add(91, 23));
+    }
+}
